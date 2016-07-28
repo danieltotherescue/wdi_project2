9f3ca24fdc18aa09a64fd52f889bcaa7b74d2078
Daniel Landau <danieltotherescue@gmail.com>
2016-07-28 15:49:22 -0700

    added paperclip gem and AWS

--- Gemfile
+++ Gemfile
@@ -25,6 +25,11 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
+gem 'paperclip', '~> 4.1'
+
+gem 'figaro'
+
+gem 'aws-sdk'
 
 gem 'devise'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,11 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'buskr'
+  }
+}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
