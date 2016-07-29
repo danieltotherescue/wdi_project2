195b5d5641ec9d66a1a0d71ed79ce9ddc86f0891
Daniel Landau <danieltotherescue@gmail.com>
2016-07-28 17:56:21 -0700

    adds default image

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+         has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTpeo_EKa2Sd9ZstbR-dKsqgLoijbeYlCMFVikOwdehcz2HV8MTDTyh1Ag"
          validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
   has_many :posts, dependent: :destroy
